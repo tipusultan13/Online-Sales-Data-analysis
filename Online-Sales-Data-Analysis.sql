f73c28ab4dcadd8e625ea45b4f5fe1dd17135347
tipusultan13 <156308034+tipusultan13@users.noreply.github.com>
2024-01-11 13:37:31 +0100

    New Upload

---
+++ Online-Sales-Data-Analysis.sql
@@ -0,0 +1,12 @@
+--create database SQL101
+--use SQL101
+
+--Data Exploration--
+--This is a online sales data of several products taken from Kaggle--
+
+select top 2 * from Details
+select top 2 * from Orders
+
+
+
+
